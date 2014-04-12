f793f9e4926069222964e5f1a725c51cd118b162
Karasandra <lykeny@yahoo.com>
2014-04-12 14:24:42 +0400

    Updates

---
+++ RRM_common/rrm/kara/RealisticRandomMod.java
@@ -0,0 +1,27 @@
+package rrm.kara;
+
+import rrm.kara.lib.Referance;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Referance.MOD_ID, name = Referance.MOD_NAME, version = Referance.VERSION)
+public class RealisticRandomMod {
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+}
---
+++ RRM_common/rrm/kara/lib/Referance.java
@@ -0,0 +1,9 @@
+package rrm.kara.lib;
+
+public class Referance {
+   
+    public static final String MOD_ID = "RRM";
+    public static final String MOD_NAME = "Realistic Random Mod";
+    public static final String VERSION = "1.7.2-0.0.1";
+    
+}
